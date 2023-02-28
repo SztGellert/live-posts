52b56d59cccc70124e0fc0b6885b5c2a713b0c26
SztGellert <sztankgellert@gmail.com>
2023-02-28 19:23:26 +0100

    use routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { PostComponent } from './post/post.component';
-import { PostEditComponent } from './post-edit/post-edit.component';
-import { AuthComponent } from './auth/auth.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {PostListComponent} from './post-list/post-list.component';
+import {PostComponent} from './post/post.component';
+import {PostEditComponent} from './post-edit/post-edit.component';
+import {AuthComponent} from './auth/auth.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: 'post-list',
+    component: PostListComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'post-add',
+    component: PostEditComponent
+  }
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,9 +34,11 @@ import { AuthComponent } from './auth/auth.component';
     AuthComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
